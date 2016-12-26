5fe902b7bf3ddbd3c362f8d2ff44a27a05aaec11
Jason <cameljeet@gmail.com>
2016-12-26 01:37:09 -0800

    Add Message/Event Router

--- main.go
+++ main.go
@@ -1,86 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "github.com/gorilla/websocket"
-  "github.com/mitchellh/mapstructure"
   "net/http"
-  "time"
 )
-
-type Message struct {
-  Name string      `json:"name"`
-  Data interface{} `json:"data"`
-}
 
 type Channel struct {
   Id   string `json:"id"`
   Name string `json:"name"`
 }
 
-var upgrader = websocket.Upgrader {
-  ReadBufferSize: 1024,
-  WriteBufferSize: 1024,
-  CheckOrigin: func (r *http.Request) bool { return true },
-}
-
 func main() {
-  http.HandleFunc("/", handler)
+  router := NewRouter()
+
+  http.Handle("/", router)
   http.ListenAndServe(":4000", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-  // fmt.Fprintf(w, "Hello AGAIN from inside Go Browser")
-  socket, err := upgrader.Upgrade(w, r, nil)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-
-  for {
-    var inMessage Message
-    var outMessage Message
-
-    if err := socket.ReadJSON(&inMessage); err != nil {
-      fmt.Println(err)
-      break
-    }
-    fmt.Printf("%#v\n", inMessage)
-
-    switch inMessage.Name {
-    case "channel add":
-      err := addChannel(inMessage.Data)
-      if err != nil {
-        outMessage = Message{"error", err}
-        if err := socket.WriteJSON(outMessage); err != nil {
-          fmt.Println(err)
-          break
-        }
-      }
-    case "channel subscribe":
-      go subscribeChannel(socket)
-    }
-  }
-}
-
-func addChannel(data interface{}) error {
-  var channel Channel
-  err := mapstructure.Decode(data, &channel)
-  if err != nil {
-    return err
-  }
-  channel.Id = "1"
-  fmt.Println("added channel")
-  return nil
-}
-
-func subscribeChannel(socket *websocket.Conn) {
-  // TODO: add in rethinkDB Query and changefeed
-  for {
-    time.Sleep(time.Second * 1)
-    message := Message{"channel add",
-      Channel{"1", "Software Support"}}
-    socket.WriteJSON(message)
-    fmt.Println("sent new channel")
-  }
 }
